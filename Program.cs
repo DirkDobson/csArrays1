32ed57fee79c2eb521144b09728dd1fee4152521
Dirk Dobson <dirkvancouver@live.com>
2018-11-26 17:59:26 -0700

    array methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace csArrays1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            // Length
+            System.Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            System.Console.WriteLine("Index of 9: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+
+            System.Console.WriteLine("Effect of Clear()");;
+            foreach (var n in numbers)
+            {
+            System.Console.WriteLine(n);
+            }
+
+            //Copy
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            System.Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+                System.Console.WriteLine(n);;
+
         }
     }
 }
